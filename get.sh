e66ba6f29239c85ed95c98a4a27717a87216ed4f
PE0SAT <janvgils@hotmail.com>
2022-08-16 18:33:04 +0200

    Add variable

--- get.sh
+++ get.sh
@@ -1,22 +1,27 @@
-${HOME}/Apps/ephemeris/jplget.sh -21
-${HOME}/Apps/ephemeris/jplget.sh -61
-${HOME}/Apps/ephemeris/jplget.sh -64
-${HOME}/Apps/ephemeris/jplget.sh -74
-${HOME}/Apps/ephemeris/jplget.sh -95
-${HOME}/Apps/ephemeris/jplget.sh -96
-${HOME}/Apps/ephemeris/jplget.sh -121
-${HOME}/Apps/ephemeris/jplget.sh -144
-${HOME}/Apps/ephemeris/jplget.sh -170
-${HOME}/Apps/ephemeris/jplget.sh -227
-${HOME}/Apps/ephemeris/jplget.sh -234
-${HOME}/Apps/ephemeris/jplget.sh -235
-${HOME}/Apps/ephemeris/jplget.sh -557
-${HOME}/Apps/ephemeris/jplget.sh -139479
-${HOME}/Apps/ephemeris/jplget.sh 299
-${HOME}/Apps/ephemeris/jplget.sh 391
-${HOME}/Apps/ephemeris/jplget.sh 392
-${HOME}/Apps/ephemeris/jplget.sh 499
-${HOME}/Apps/ephemeris/jplget.sh 599
-${HOME}/Apps/ephemeris/jplget.sh 699
-${HOME}/Apps/ephemeris/jplget.sh 101955
-${HOME}/Apps/ephemeris/jplget.sh 162173
+#!/usr/bin/env bash
+#
+
+TOPDIR="${HOME}"/Apps/ephemeris # modify this line with your directory
+
+"${TOPDIR}"/jplget.sh -21
+"${TOPDIR}"/jplget.sh -61
+"${TOPDIR}"/jplget.sh -64
+"${TOPDIR}"/jplget.sh -74
+"${TOPDIR}"/jplget.sh -95
+"${TOPDIR}"/jplget.sh -96
+"${TOPDIR}"/jplget.sh -121
+"${TOPDIR}"/jplget.sh -144
+"${TOPDIR}"/jplget.sh -170
+"${TOPDIR}"/jplget.sh -227
+"${TOPDIR}"/jplget.sh -234
+"${TOPDIR}"/jplget.sh -235
+"${TOPDIR}"/jplget.sh -557
+"${TOPDIR}"/jplget.sh -139479
+"${TOPDIR}"/jplget.sh 299
+"${TOPDIR}"/jplget.sh 391
+"${TOPDIR}"/jplget.sh 392
+"${TOPDIR}"/jplget.sh 499
+"${TOPDIR}"/jplget.sh 699
+"${TOPDIR}"/jplget.sh 599
+"${TOPDIR}"/jplget.sh 101955
+"${TOPDIR}"/jplget.sh 162173
